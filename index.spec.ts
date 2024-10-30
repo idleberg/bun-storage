7af513580ec9f094adf31d922ff23f28f51473f0
Jan T. Sott <git@idleberg.com>
2024-10-30 21:56:00 +0100

    use .ts import

--- index.spec.ts
+++ index.spec.ts
@@ -1,5 +1,5 @@
 import { beforeEach, expect, test } from 'bun:test';
-import { createLocalStorage, createSessionStorage } from './index.js';
+import { createLocalStorage, createSessionStorage } from './index.ts';
 
 // Helpers
 import { randomUUID } from "node:crypto";
