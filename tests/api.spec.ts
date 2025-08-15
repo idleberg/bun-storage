7ae1a138ca25040a527ec74acde0e4d5cf793a96
Jan T. Sott <git@idleberg.com>
2025-08-15 22:44:48 +0200

    ci: add workaround to use local biome instance

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -3,10 +3,9 @@ import { beforeEach, expect, test } from 'bun:test';
 import { randomUUID } from 'node:crypto';
 import { tmpdir } from 'node:os';
 import { resolve } from 'node:path';
-import { createLocalStorage, createSessionStorage, createStorages } from '../index.ts';
+import { createLocalStorage, createSessionStorage, createStorages, Storage } from '../index.ts';
 
 const dbFile = resolve(tmpdir(), `${randomUUID()}.sqlite`);
-
 const storages = createStorages(dbFile);
 
 [
